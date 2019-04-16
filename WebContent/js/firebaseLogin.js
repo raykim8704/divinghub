17ddc21365d1489b1d8736e6b71afef73929e5e0
KwanghoonKim <raykim8704@lotte.net>
2019-04-16 20:00:39 +0900

    google login

--- WebContent/js/firebaseLogin.js
+++ WebContent/js/firebaseLogin.js
@@ -515,6 +515,35 @@ loading.out();
     });
 
   },
+  registerWithGoogle : function(){
+    var provider = new firebase.auth.GoogleAuthProvider();
+
+    return new Promise (function(resolve,reject){
+      firebase.auth().signInWithPopup(provider).then(function(result) {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        var token = result.credential.accessToken;
+        // The signed-in user info.
+        var user = result.user;
+        console.log('result : ', result);
+        resolve(result)
+        // ...
+      }).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // The email of the user's account used.
+        var email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
+        reject(error)
+        // ...
+      });
+    })
+
+
+
+
+  },
   getPhoneAuth : function(phoneNumber){
     window.recaptchaVerifier = new firebase.auth.RecaptchaVerifier('recaptcha-container');
     var appVerifier = window.recaptchaVerifier;
